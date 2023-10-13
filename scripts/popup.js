7c88e893ac6dd75f0995585f04b81ca404f25f74
chloetouzani <chloe.touzani11@gmail.com>
2023-10-13 14:50:52 +0200

    fin du projet

---
+++ scripts/popup.js
@@ -0,0 +1,26 @@
+function afficherPopup() {
+    let popupBackground = document.querySelector(".popupBackground")
+    popupBackground.classList.add("active")
+}
+
+function cacherPopup() {
+    let popupBackground = document.querySelector(".popupBackground")
+    popupBackground.classList.remove("active")
+}
+
+
+function initAddEventListenerPopup() {
+let popupBackground = document.querySelector(".popupBackground")
+btnPartage = document.querySelector(".score button")
+btnPartage.addEventListener("click", () => {
+    afficherPopup()
+})
+popupBackground.addEventListener("click", (event) => {
+    if (event.target === popupBackground) {
+        cacherPopup()
+    }
+})}
+
+
+
+
--- scripts/script.js
+++ scripts/script.js
@@ -9,26 +9,53 @@ function newWord (userWord) {
     givenWord.innerText = userWord
 }
 
+function afficherEmail(name, mail, score) {
+    let mailto = `mailto:${mail}?subject=Partage du score Azertype&body=Salut, je suis ${name} et je viens de réaliser le score ${score} sur le site d'Azertype !`
+    location.href = mailto
+}
+
+function validateName(name) {
+    if (name.length >=2) {
+        return true
+    }
+    return false
+}
+
+function validateMail(mail) {
+    let regexMail = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+")
+
+    if (regexMail.test(mail)) {
+        return true
+    }
+    return false
+}
+
 function runGame () {
-    let score = 0
-    let i = 0
-    let numberOfWords = 0
-    let choiceList = APP_WORDS
-    let validateWord = document.querySelector(".input button")
-    let userText = document.querySelector("#given-word-input")
-    let wordChoice = document.querySelectorAll(".choice input")
+    initAddEventListenerPopup()
+    let score           = 0
+    let i               = 0
+    let numberOfWords   = 0
+    let choiceList      = APP_WORDS
+
+    let validateWord    = document.querySelector(".input button")
+    let userText        = document.querySelector("#given-word-input")
+    let wordChoice      = document.querySelectorAll(".choice input")
+    let submitButton    = document.querySelector(".popup form")
 
     validateWord.addEventListener("click", () => {
         console.log(`jai cliqué et écrit ${userText.value}`)
+
         if (choiceList[i] === userText.value) {
             score++
         }
+
         i++
         console.log(choiceList[i])
         newWord (choiceList[i])
         userText.value= ""
         numberOfWords++
         returnScoreMessage (score, numberOfWords)
+
         if (choiceList[i] === undefined) {
         newWord("Le jeu est fini")
         validateWord.setAttribute("disabled", true)
@@ -36,8 +63,8 @@ function runGame () {
             newWord (choiceList[i])
         }
         })
-    returnScoreMessage (score, numberOfWords)
 
+    returnScoreMessage (score, numberOfWords)
 
         for (let index = 0; index < wordChoice.length; index++) {
             wordChoice[index].addEventListener("change", (event) => {
@@ -48,9 +75,21 @@ function runGame () {
                     choiceList = APP_SENTENCES
                 }
                 newWord(choiceList[i])
-    })
-}
-}
+            })}
+
+            submitButton.addEventListener("submit", (event) => {
+                event.preventDefault();
+
+                let name            = document.querySelector("#nom").value
+                let mail            = document.querySelector("#email").value
+
+                if (validateName(name) && validateMail(mail)) {
+                    afficherEmail(name, mail, score)
+                } else {
+                console.log("erreur")
+            }
+            })}
+
 
 function share() {
     let shareButton = document.querySelector("form")
